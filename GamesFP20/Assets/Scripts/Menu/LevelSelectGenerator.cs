3cb52acc83d74f6d56d06d83de42b1298e31015b
robinfleige <robin-fleige@web.de>
2020-06-08 11:30:19 +0200

    Create Basic Menu Scene

---
+++ GamesFP20/Assets/Scripts/Menu/LevelSelectGenerator.cs
@@ -0,0 +1,9 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectGenerator : MonoBehaviour
+{
+	
+}
---
+++ GamesFP20/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject game;
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void StartGame()
+	{
+
+	}
+}
