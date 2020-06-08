4893bacd731646590f9ea4b9c67784d869e65110
robinfleige <robin-fleige@web.de>
2020-06-08 12:00:30 +0200

    Added Basic Game Scene with Basic Player Movement

---
+++ GamesFP20/Assets/Scripts/Game/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 6;
+	public float jumpForce = 10;
+	public float gravity = 20;
+	private CharacterController controller;
+
+	void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+	{
+        Vector3 velocity = controller.velocity;
+
+		velocity.x = Input.GetAxis("Horizontal") * speed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			velocity.y = jumpForce;
+		}
+		
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime); 
+    }
+}
