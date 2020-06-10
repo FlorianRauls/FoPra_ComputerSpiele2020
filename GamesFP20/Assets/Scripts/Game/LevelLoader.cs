5f5a7273377d7b46bac9651ac575886e288d3376
robinfleige <robin-fleige@web.de>
2020-06-10 14:25:45 +0200

    Changed structure to use Resource.load and edit game scene to fit loading of all parts

---
+++ GamesFP20/Assets/Scripts/Game/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject boy = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        GameObject level = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Level/Level0"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GamesFP20/Assets/Scripts/Menu/LevelSelectGenerator.cs
+++ GamesFP20/Assets/Scripts/Menu/LevelSelectGenerator.cs
@@ -1,7 +1,4 @@
-﻿using TMPro;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class LevelSelectGenerator : MonoBehaviour
 {
