faa5dec9916f4574cdb2c6182f5bbc3a03058592
robinfleige <robin-fleige@web.de>
2020-06-10 19:03:26 +0200

    Added Level Selection and remplaced TextMeshPro with simple Text Object

--- GamesFP20/Assets/Scripts/Game/LevelLoader.cs
+++ GamesFP20/Assets/Scripts/Game/LevelLoader.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+    public static int levelIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
         GameObject boy = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
-        GameObject level = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Level/Level0"));
+        GameObject level = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Level/Level"+levelIndex));
     }
 
     // Update is called once per frame
--- GamesFP20/Assets/Scripts/Menu/LevelSelectGenerator.cs
+++ GamesFP20/Assets/Scripts/Menu/LevelSelectGenerator.cs
@@ -1,6 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectGenerator : MonoBehaviour
 {
-	
+	public GameObject content;
+	public GameObject levelSelectorPrefab;
+
+	void Start()
+	{
+		var rect = GetComponent<RectTransform>();
+
+		for (int i = 0; Resources.Load<GameObject>($"Prefabs/Level/Level{i}") != null; i++)
+		{
+			var selector = Instantiate(levelSelectorPrefab, content.transform);
+			selector.transform.localPosition = new Vector3(0, -40 - i * 50, 0);
+
+			int level = i;
+			selector.GetComponentInChildren<Button>().onClick.AddListener(
+				() =>
+				{
+					LevelLoader.levelIndex = level;
+					SceneManager.LoadScene("Game");
+				});
+		}
+	}
 }
