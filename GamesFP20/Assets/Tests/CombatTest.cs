2f126a0381ddf613ed77dcd004ce1a71e4c09a75
Florian Rauls <34367590+FlorianRauls@users.noreply.github.com>
2020-08-28 12:08:39 +0200

    Update CombatTest.cs
    Finished Two Tests

--- GamesFP20/Assets/Tests/CombatTest.cs
+++ GamesFP20/Assets/Tests/CombatTest.cs
@@ -11,30 +11,54 @@ public class CombatTest : MonoBehaviour
     [Test]
     public void TestOpenDefeatMenuOnDefeat()
     {
+        GameObject boyObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        Player boy = boyObject.GetComponent<Player>();
+        boy.Start();
+
+        boy.Defeat();
+
+        Assert.AreEqual(boy.getDefeatMenuOpen(), true);
 
     }
 
     [Test]
     public void TestDefeatOnCollisionWithEnemy()
     {
+        GameObject boyObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        Player boy = boyObject.GetComponent<Player>();
+        boy.Start();
+
+        GameObject projectileObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Projectile"));
         
+        boy.collide(projectileObject);
+
+        Assert.AreEqual(boy.getDefeated(), true);
     }
 
     [Test]
     public void TestDisableMovementOnDefeat()
     {
+        GameObject boyObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        Player boy = boyObject.GetComponent<Player>();
+        boy.Start();
 
     }
 
     [Test]
     public void TestUndefeatableInLevelMenu()
     {
+        GameObject boyObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        Player boy = boyObject.GetComponent<Player>();
+        boy.Start();
 
     }
 
     [Test]
     public void TestUndefeatableInLevelAccomplisedMenu()
     {
-        
+        GameObject boyObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Boy"));
+        Player boy = boyObject.GetComponent<Player>();
+        boy.Start();
+
     }
 }
