ad713e4ad80e6be76b2cb3595b88cbf912eb2a77
robinfleige <robin-fleige@web.de>
2020-08-29 20:42:54 +0200

    Missing files from last commit

---
+++ GamesFP20/Assets/Tests/LevelLoaderTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LevelLoaderTest
+    {
+        [Test]
+        public void StartTest()
+        {
+            LevelLoader levelLoader = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/LevelLoader")).GetComponent<LevelLoader>();
+            levelLoader.Start();
+            Assert.IsTrue(levelLoader.GetCurrentLevel() != null);
+            Assert.IsTrue(levelLoader.GetCurrentDividerLevel() != null);
+        }
+
+        [Test]
+        public void NextLevelTest()
+        {
+            LevelLoader levelLoader = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/LevelLoader")).GetComponent<LevelLoader>();
+            levelLoader.Start();
+            Level lastLevel = levelLoader.GetCurrentLevel();
+            Level lastLevelDivider = levelLoader.GetCurrentDividerLevel();
+            int lastLevelID = LevelLoader.levelIndex;
+            levelLoader.LoadNextLevel();
+            Assert.IsTrue(levelLoader.GetCurrentLevel() != lastLevel);
+            Assert.IsTrue(levelLoader.GetCurrentDividerLevel() != lastLevelDivider);
+            Assert.IsTrue(LevelLoader.levelIndex != lastLevelID);
+        }
+    }
+}
