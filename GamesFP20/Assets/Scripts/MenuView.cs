cdce1a52b113de7052e85f2dfc13250a6092c229
robinfleige <robin-fleige@web.de>
2020-09-27 19:08:55 +0200

    Added Unity workaround to make new attempt work

--- GamesFP20/Assets/Scripts/MenuView.cs
+++ GamesFP20/Assets/Scripts/MenuView.cs
@@ -23,4 +23,39 @@ public class MenuView : MonoBehaviour
     {
         MenuManager.GetInstance().Back();
     }
+
+    //Following functions are only workarounds, because Unity does not allow functions with Enum Parameters
+
+    public void TransitionToDummy()
+    {
+        TransitionTo(MenuEnum.Dummy);
+    }
+    public void TransitionToMain()
+    {
+        TransitionTo(MenuEnum.Main);
+    }
+    public void TransitionToLevelSelect()
+    {
+        TransitionTo(MenuEnum.LevelSelect);
+    }
+    public void TransitionToProfileSelect()
+    {
+        TransitionTo(MenuEnum.ProfileSelect);
+    }
+    public void TransitionToProfileDetail()
+    {
+        TransitionTo(MenuEnum.ProfileDetail);
+    }
+    public void TransitionToGamePause()
+    {
+        TransitionTo(MenuEnum.GamePause);
+    }
+    public void TransitionToGameDeath()
+    {
+        TransitionTo(MenuEnum.GameDeath);
+    }
+    public void TransitionToGameFinish()
+    {
+        TransitionTo(MenuEnum.GameFinish);
+    }
 }
