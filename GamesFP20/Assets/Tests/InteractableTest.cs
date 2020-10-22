459ea05911d1b36dfbddef6553124ecb2030d1ed
Florian Rauls <frauls@web.de>
2020-10-22 19:55:55 +0200

    Wrong Folder
    Moved InteractableTest.cs

---
+++ GamesFP20/Assets/Tests/InteractableTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Tests
+{
+    public class InterActableTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
